4cef4df63977f605f72c01ab115574d62e37723d
khangng72 <khangnguyn07022002@gmail.com>
2024-09-27 09:36:47 +0700

    spring initializr

---
+++ src/main/java/hcmut/contentCreatorOnline/ContentCreatorOnlineApplication.java
@@ -0,0 +1,13 @@
+package hcmut.contentCreatorOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCreatorOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentCreatorOnlineApplication.class, args);
+	}
+
+}
