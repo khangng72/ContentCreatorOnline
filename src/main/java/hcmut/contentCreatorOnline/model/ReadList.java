8746e9791096c753c7ff4ceaf4667c841aa70daa
Vũ Trường Khoa <135007011+VuTruongKhoa@users.noreply.github.com>
2024-11-13 11:04:58 +0700

    create model chapter, comment, creation, paragraph and create relation

--- src/main/java/hcmut/contentCreatorOnline/model/ReadList.java
+++ src/main/java/hcmut/contentCreatorOnline/model/ReadList.java
@@ -4,10 +4,6 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
-
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.UUID;
 
