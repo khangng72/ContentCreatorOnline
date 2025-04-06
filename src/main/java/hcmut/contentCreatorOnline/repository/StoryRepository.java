cb32da7e427f676b4701d06802a16797f2c07db5
Vũ Trường Khoa <135007011+VuTruongKhoa@users.noreply.github.com>
2025-04-06 16:54:24 +0700

    create api to get all story by user_id (owner)

--- src/main/java/hcmut/contentCreatorOnline/repository/StoryRepository.java
+++ src/main/java/hcmut/contentCreatorOnline/repository/StoryRepository.java
@@ -2,6 +2,8 @@ package hcmut.contentCreatorOnline.repository;
 
 import hcmut.contentCreatorOnline.model.Story;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,6 @@ import java.util.UUID;
 @Repository
 public interface StoryRepository extends JpaRepository<Story, UUID> {
     List<Story> findByUserPost_Id(UUID userId);
+    @Query("SELECT s FROM Story s JOIN s.userOwn u WHERE u.id = :userId")
+    List<Story> findByUserOwn_Id(@Param("userId") UUID userId);
 }
