67a0b6bd4f2d14603b222d9b193f6352051ebb6e
Javu-N <121878933+Javu-N@users.noreply.github.com>
2025-04-07 11:57:58 +0700

    Revert "Feature/cco 39"

--- src/main/java/hcmut/contentCreatorOnline/controller/chapter/ChapterController.java
+++ src/main/java/hcmut/contentCreatorOnline/controller/chapter/ChapterController.java
@@ -1,20 +1,15 @@
 package hcmut.contentCreatorOnline.controller.chapter;
 
 import hcmut.contentCreatorOnline.dto.chapter.ChapterRequest;
+import hcmut.contentCreatorOnline.dto.chapter.CreateNewChapterStoryResponse;
 import hcmut.contentCreatorOnline.dto.chapter.NewChapterResponseDTO;
 import hcmut.contentCreatorOnline.model.Chapter;
-import hcmut.contentCreatorOnline.model.Story;
-import hcmut.contentCreatorOnline.repository.ChapterRepository;
-import hcmut.contentCreatorOnline.repository.StoryRepository;
 import hcmut.contentCreatorOnline.service.impl.ChapterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -23,8 +18,8 @@ import java.util.UUID;
 public class ChapterController {
     private final ChapterService chapterService;
 
-    @PostMapping("/newChapter/{storyId}")
-    public ResponseEntity<Map<String, Object>> createNewChapter(
+    @PostMapping("/new/{storyId}")
+    public ResponseEntity<CreateNewChapterStoryResponse> createNewChapterStory(
             @PathVariable("storyId") UUID storyId,
             @RequestBody ChapterRequest chapterRequest) {
 
@@ -32,12 +27,9 @@ public class ChapterController {
 
         NewChapterResponseDTO responseDTO = new NewChapterResponseDTO(
                 newChapter.getChapterId(),
-                newChapter.getStory().getStoryId()
-        );
+                newChapter.getStory().getStoryId());
 
-        Map<String, Object> response = new HashMap<>();
-        response.put("status", 201);
-        response.put("result", responseDTO);
+        CreateNewChapterStoryResponse response = new CreateNewChapterStoryResponse(HttpStatus.CREATED.value(), responseDTO);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
--- src/main/java/hcmut/contentCreatorOnline/repository/StoryRepository.java
+++ src/main/java/hcmut/contentCreatorOnline/repository/StoryRepository.java
@@ -4,11 +4,9 @@ import hcmut.contentCreatorOnline.model.Story;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface StoryRepository extends JpaRepository<Story, UUID> {
-    // Tìm tất cả các truyện của tác giả theo user_id (user_post_id)
-    List<Story> findByUserPost_Id(UUID userId);
+
 }
