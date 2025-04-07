dd9b170362c1e703f8b7b16b549b39bbe7c6fe11
khangng72 <khangnguyn07022002@gmail.com>
2025-04-07 20:38:57 +0700

    feat(CCO-39): add api to get all stories by user_id and api by story_id

--- src/main/java/hcmut/contentCreatorOnline/controller/story/StoryController.java
+++ src/main/java/hcmut/contentCreatorOnline/controller/story/StoryController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -37,5 +38,16 @@ public class StoryController {
         return new ResponseEntity<>(new UpdateStoryGenreResponse(HttpStatus.OK.value(), result), HttpStatus.OK);
 
     }
-}
 
+    @GetMapping("/post/{user_id}")
+    public ResponseEntity<List<StoryResponse>> getStoriesPostedByUser(@PathVariable UUID user_id) {
+        List<StoryResponse> storyResponses = storyService.getStoriesPostedByUser(user_id);
+        return ResponseEntity.ok(storyResponses);
+    }
+
+    @GetMapping("/{story_id}")
+    public ResponseEntity<StoryResponse> getStoryByStoryId(@PathVariable UUID story_id) {
+        StoryResponse storyDTO = storyService.getStoryByStoryId(story_id);
+        return ResponseEntity.ok(storyDTO);
+    }
+}
--- src/main/java/hcmut/contentCreatorOnline/repository/StoryRepository.java
+++ src/main/java/hcmut/contentCreatorOnline/repository/StoryRepository.java
@@ -4,9 +4,10 @@ import hcmut.contentCreatorOnline.model.Story;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface StoryRepository extends JpaRepository<Story, UUID> {
-
+    List<Story> findByUserPost_Id(UUID userId);
 }
