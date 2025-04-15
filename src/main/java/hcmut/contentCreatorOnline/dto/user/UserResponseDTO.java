0ddb71d172a96231c4cdfe3bc4df06ed47b2818d
Vũ Trường Khoa <135007011+VuTruongKhoa@users.noreply.github.com>
2025-04-16 06:58:36 +0700

    create api to get user by id

---
+++ src/main/java/hcmut/contentCreatorOnline/dto/user/UserResponseDTO.java
@@ -0,0 +1,23 @@
+package hcmut.contentCreatorOnline.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class UserResponseDTO {
+    private UUID id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private boolean isAdmin;
+    private boolean isActive;
+    private String nationality;
+    private LocalDate birthday;
+}
