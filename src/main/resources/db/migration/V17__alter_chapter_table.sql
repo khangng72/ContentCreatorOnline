9d8f5ea807fc3b13df1868aeb88d4b53cb465db7
khangng72 <khangnguyn07022002@gmail.com>
2025-04-20 01:34:45 +0700

    add trigger

--- src/main/resources/db/migration/V17__alter_chapter_table.sql
+++ src/main/resources/db/migration/V17__alter_chapter_table.sql
@@ -8,4 +8,22 @@ CREATE TABLE user_like_chapter
     PRIMARY KEY (user_id, chapter_id),
     FOREIGN KEY (user_id) REFERENCES cco_user (user_id) ON DELETE CASCADE,
     FOREIGN KEY (chapter_id) REFERENCES chapter (chapter_id) ON DELETE CASCADE
-)
+);
+
+CREATE OR REPLACE FUNCTION update_chapter_like_count()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    UPDATE Chapter
+    SET number_of_likes = number_of_likes + 1
+    WHERE chapter_id = NEW.chapter_id;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_update_chapter_like_count
+    AFTER INSERT
+    ON user_like_chapter
+    FOR EACH ROW
+EXECUTE FUNCTION update_chapter_like_count();
