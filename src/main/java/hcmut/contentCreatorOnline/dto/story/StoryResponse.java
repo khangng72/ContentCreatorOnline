1acaa1892162d4e0865af51b4813eff9fe1bbd35
khangng72 <khangnguyn07022002@gmail.com>
2025-04-26 19:26:26 +0700

    modify story response

--- src/main/java/hcmut/contentCreatorOnline/dto/story/StoryResponse.java
+++ src/main/java/hcmut/contentCreatorOnline/dto/story/StoryResponse.java
@@ -1,10 +1,9 @@
 package hcmut.contentCreatorOnline.dto.story;
 
-import hcmut.contentCreatorOnline.dto.user.UserResponseDTO;
-import hcmut.contentCreatorOnline.model.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 
@@ -14,46 +13,31 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class StoryResponse {
+    private UUID storyId;
+    
     private LocalDate releaseDate;
 
     private LocalDate createdDate;
 
-
     private Boolean releaseStatus;
-
 
     private String storyTitle;
 
-
     private Boolean saleOnly;
-
 
     private Double salePrice;
 
-
     private Integer numberOfLikes;
-
 
     private String coverImageUri;
 
-
     private String storyDescription;
-
 
     private String tags;
 
     private Double averageRating;
 
-//    private List<Chapter> chapters;
-
-//    private Set<Genre> genres = new HashSet<>();
-//
-//    private Set<ReadList> readLists = new HashSet<>();
-//
-//    private Set<Order> orders = new HashSet<>();
-
-
-//    private User userPost;
-
     private UUID userId;
+
+    private List<ChapterStoryResponse> chapters;
 }
