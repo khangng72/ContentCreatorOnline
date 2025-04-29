7c2bcff6c049571053ef2bef8aa57a6d7838504c
khangng72 <khangnguyn07022002@gmail.com>
2025-04-29 23:57:09 +0700

    fix: not null warning

--- src/main/java/hcmut/contentCreatorOnline/repository/GenreRepository.java
+++ src/main/java/hcmut/contentCreatorOnline/repository/GenreRepository.java
@@ -1,6 +1,7 @@
 package hcmut.contentCreatorOnline.repository;
 
 import hcmut.contentCreatorOnline.model.Genre;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Integer> {
-    Optional<Genre> findById(Integer genreId);
+    @NotNull
+    Optional<Genre> findById(@NotNull Integer genreId);
 
 }
