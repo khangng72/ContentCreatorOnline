4f4344a68b06cd440c6d657ef45c2fdc2fa41c64
khangng72 <khangnguyn07022002@gmail.com>
2025-05-03 12:30:44 +0700

    feat: add random avatar for user

---
+++ src/main/resources/db/migration/V37__set_sample_avatar.sql
@@ -0,0 +1,270 @@
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar5.png'
+WHERE user_id = '53b10783-d693-4c2d-9815-2b72fcdb53d3';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar5.png'
+WHERE user_id = 'c69f7ae0-0e87-4b62-93ae-099233ebf9fd';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar5.png'
+WHERE user_id = '8acbe838-6597-421d-9df9-b3b83cb7fc14';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar3.png'
+WHERE user_id = '2a2a8ad1-35d8-460b-a6a2-f41b3d134ae5';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar1.png'
+WHERE user_id = '7d3a00d5-f547-4308-84f0-cb7cb8b3eb87';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar6.png'
+WHERE user_id = 'f6b24e76-2cd6-4881-86e9-ffadbba23720';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar5.png'
+WHERE user_id = 'fd81d17f-8ccb-4a65-a281-0a2d8ba25b2e';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar2.png'
+WHERE user_id = '061460f1-45fd-40a8-b243-5da3f0f35523';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = '0b4b5fd4-5b49-4a8f-a325-3fa42fc0d85c';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = '3a2e7c0f-2bd3-4b29-8e80-5e3b6e65fdb2';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar4.png'
+WHERE user_id = '51ff87c6-7ff5-4af2-bb0a-6ffcc103e135';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar4.png'
+WHERE user_id = '13d4f6cf-8d2d-4c78-89e5-b16314ee6e07';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = 'fd3e2b9d-1ff1-48a8-b1db-5e20f6939a7e';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar3.png'
+WHERE user_id = '3b4c8e19-c961-4d3d-86a8-8bde55c63fc5';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar4.png'
+WHERE user_id = 'e9f6b882-6fc2-48e7-a948-c65b5b7654a3';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar4.png'
+WHERE user_id = 'ac39145d-73f6-46e0-8493-2b3c5ffb7b41';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar4.png'
+WHERE user_id = '7f9d70a7-cd6f-462b-8c5c-5c0b86ea4044';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar6.png'
+WHERE user_id = '5ed82af6-6e59-4a7d-9336-19ec4962cf62';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar3.png'
+WHERE user_id = '1fd4bc9f-6a96-4c6a-802d-23b6cb7aa45a';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar3.png'
+WHERE user_id = 'b6c967d2-f5e9-4a77-b0a5-5dc8c79dbe27';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = 'd35b1ad9-b497-43ec-94ae-73651dfac235';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar2.png'
+WHERE user_id = '5c529b4d-cf68-4f60-9d6a-b3ebc9f4b9e9';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = 'cf145e7b-d67b-4b09-b350-b0651ee53b39';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar1.png'
+WHERE user_id = 'eb21b92b-23a3-41e9-b76f-16871984340f';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar2.png'
+WHERE user_id = 'f503d21c-91a8-4b41-a0db-68b7d4c6cf9e';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar3.png'
+WHERE user_id = 'f26f740c-1160-4b77-b6d4-7926a73f2ab3';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = 'dceab054-03c7-4b6c-8d5d-63b065f8e58e';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = 'e77d7ff6-25f4-4472-812e-cb75e9432d13';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar1.png'
+WHERE user_id = 'b65087c2-4cb1-4593-b074-7db9a343abb8';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar1.png'
+WHERE user_id = 'bf92ec2a-2467-4b8e-97ef-c2df74b4d31e';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar5.png'
+WHERE user_id = 'ff2116f9-e1f4-43cf-addb-c2667887fad0';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar4.png'
+WHERE user_id = 'adb3bd5e-6a00-4b8b-9a73-69d93934d226';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar6.png'
+WHERE user_id = 'ff00d698-2d51-49c7-aa1d-c1794d72328d';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar4.png'
+WHERE user_id = '8acf7076-3c79-4187-83c0-9a3e47e9b242';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar8.png'
+WHERE user_id = '0961c1b9-530d-4ce2-99c2-e7d0e9533d95';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar5.png'
+WHERE user_id = '96827a6b-6ad7-4153-91d9-fa36b922d4a1';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar5.png'
+WHERE user_id = '104dac49-051f-4bdb-a3cc-3d1260ff194d';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar2.png'
+WHERE user_id = 'fc489c18-ee49-4c2b-973f-1f853f478eff';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar3.png'
+WHERE user_id = '3a8de908-3497-4b4d-a5ea-54051dc2d05a';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar3.png'
+WHERE user_id = 'b9d5f979-26c1-4f5c-8a18-fae558d77f8f';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar5.png'
+WHERE user_id = '29f76b11-ac0e-4c3a-8708-0ca6f3bac718';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = '054a69bd-b860-4fb1-a458-8c421ca83d55';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar2.png'
+WHERE user_id = '59c6495c-665d-40fa-a964-00a556620bfe';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = '3b2fb2df-ea78-4e9d-b140-371dc83784bf';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar1.png'
+WHERE user_id = 'b1e16f99-0188-414b-b41b-d7ee5e320061';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar4.png'
+WHERE user_id = 'dc853c87-d089-4f52-9f03-2741a8455c7b';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar2.png'
+WHERE user_id = 'f9d864d4-d82b-4543-914d-73e6d979b6d9';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar3.png'
+WHERE user_id = '789f8570-03c9-4836-b1d8-6157aa185913';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar8.png'
+WHERE user_id = 'f5096780-8186-4084-b79a-c52035d863e7';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar1.png'
+WHERE user_id = '9fdabb1f-721f-4554-b8a8-354c1164d1df';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar8.png'
+WHERE user_id = 'eed61f62-8977-490e-a625-8719adc364d0';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar4.png'
+WHERE user_id = '7e3ba039-6d8a-4adf-b161-f7fbdd3c4108';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = '1ef8106f-1092-4156-9d38-f842d49beba5';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar4.png'
+WHERE user_id = 'e75c0583-0a6c-4e2a-9915-f2b464642f70';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar1.png'
+WHERE user_id = '1ae4d330-f611-4323-9973-a1c8342192f1';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar3.png'
+WHERE user_id = '119f9c77-ff62-4bfc-a2bd-7dd3daf01acb';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar8.png'
+WHERE user_id = 'f85c666c-8306-4ca8-8216-47fb3290a223';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar1.png'
+WHERE user_id = 'f02b4646-3a12-4357-8d66-93b0f80be02f';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar2.png'
+WHERE user_id = '342f363a-b9ab-4525-84a3-1f67441c326c';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar4.png'
+WHERE user_id = '53af1858-f1f4-4ac2-a3e4-281bef6db5c4';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = 'f104841e-4087-45eb-964e-2738bb63d2ab';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar6.png'
+WHERE user_id = '8da42257-f5dd-425a-9bc9-4aacaab2f98d';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar6.png'
+WHERE user_id = '81ce98eb-8c32-4ea8-878b-78cb81ff18de';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar4.png'
+WHERE user_id = 'e26aefb0-bbfe-4fe5-9279-169bb24ff764';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = '1b65aafd-d3ca-41bb-869a-27c8cd565bef';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar3.png'
+WHERE user_id = 'bf8de320-c8f8-43da-a01c-e1ad33a6bea2';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar6.png'
+WHERE user_id = '709d657d-6ebf-4193-9751-689e2446fa62';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar1.png'
+WHERE user_id = 'bb14293f-6efe-4e75-af3d-b72da875879b';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar2.png'
+WHERE user_id = '2a8fddee-7c13-4875-b489-0e2a0dffc9cd';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar6.png'
+WHERE user_id = '354bc89e-d568-4e13-b841-40a54bd93ab2';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar8.png'
+WHERE user_id = 'bf1340ce-2ffb-4a56-b234-d9ba7a07768c';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar5.png'
+WHERE user_id = 'ebe0871d-5d67-4ece-a9f3-852c63fdf3ff';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar5.png'
+WHERE user_id = 'b4695671-4a63-4298-9932-b61a25ecf25b';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = 'c7c5b86d-da25-476a-bf4c-4f09b5459369';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar8.png'
+WHERE user_id = 'acc5c459-bdba-438e-ad2b-2c71d00e4b7c';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar1.png'
+WHERE user_id = 'aa972a05-abe9-4347-bebd-87b283ffdeee';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = '90688a5f-3832-450b-9d08-cdc01f76b0ad';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = 'b9ef5ca0-c7f9-404e-8e52-b98ce402ad39';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar4.png'
+WHERE user_id = 'cb00add0-0cea-4005-8c5c-0750325b3411';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar2.png'
+WHERE user_id = '3999adec-2896-414e-b5ab-fa408661268d';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar2.png'
+WHERE user_id = '063666bd-6ecb-440d-b560-dc306493fe59';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar3.png'
+WHERE user_id = '3df592a4-daa8-4819-863e-d4f7998ad0f3';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = '7826ee10-98d3-4cdc-96a6-2dfcdcb728d8';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar5.png'
+WHERE user_id = 'a8bcd7e3-d273-49c0-a226-259b02658c73';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar3.png'
+WHERE user_id = 'add7566f-c8c9-4932-b165-7ce6a465b18e';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = 'b5b8dbce-7d66-4399-868c-cde35c31c10a';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar8.png'
+WHERE user_id = 'd41ba763-57b5-4f8a-bdd2-2f3cc53b93a2';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar1.png'
+WHERE user_id = 'dab3e0c6-6b94-426e-8347-14a8fea8a42b';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar3.png'
+WHERE user_id = 'e6d93d32-9167-4f25-825c-33b28cf3f4c7';
+UPDATE cco_user
+SET avatar_url = '/uploads/sample_avatar/avatar7.png'
+WHERE user_id = '3092c2ea-3cf7-4211-987c-5d1d09f4a01a';
